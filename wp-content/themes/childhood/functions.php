9fae433ee758f0b3f35e2b4f47a8680b32eeb000
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2023-07-31 18:50:53 +0300

    upload project files

---
+++ wp-content/themes/childhood/functions.php
@@ -0,0 +1,13 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'childhood_styles' );
+add_action( 'wp_enqueue_scripts', 'childhood_scripts' );
+
+function childhood_styles() {
+  wp_enqueue_style( 'childhood-style', get_stylesheet_uri() );
+  wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/styles/main.min.css', 'childhood-style', time(), 'all' );
+}
+
+function childhood_scripts() {
+  wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true );
+}
+?>
