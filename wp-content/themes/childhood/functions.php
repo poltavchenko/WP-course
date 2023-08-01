8acce7766b499a722ce88b8f7d239da52743816c
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2023-08-01 17:24:10 +0300

    add logo and title

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -1,13 +1,16 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'childhood_styles' );
-add_action( 'wp_enqueue_scripts', 'childhood_scripts' );
+  add_action( 'wp_enqueue_scripts', 'childhood_styles' );
+  add_action( 'wp_enqueue_scripts', 'childhood_scripts' );
 
-function childhood_styles() {
-  wp_enqueue_style( 'childhood-style', get_stylesheet_uri() );
-  wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/styles/main.min.css', 'childhood-style', time(), 'all' );
-}
+  function childhood_styles() {
+    wp_enqueue_style( 'childhood-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/styles/main.min.css', 'childhood-style', time(), 'all' );
+  }
 
-function childhood_scripts() {
-  wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true );
-}
+  function childhood_scripts() {
+    wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true );
+  }
+
+  add_theme_support( 'custom-logo' );
+
 ?>
