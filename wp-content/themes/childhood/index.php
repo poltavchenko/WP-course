c54d2f9b96d8ec2e34f0770c78c815f655b821e0
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2023-08-02 18:30:09 +0300

    add acf plugin

--- wp-content/themes/childhood/index.php
+++ wp-content/themes/childhood/index.php
@@ -69,24 +69,35 @@
   </div>
 
   <div class="about" id="about">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-10 offset-md-1 col-lg-5 offset-lg-1">
-                  <div class="about__img">
-                      <img src="<?php echo bloginfo('template_url'); ?>/assets/img/about.jpg" alt="про компанию">
-                  </div>
-              </div>
-              <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-6 col-xl-5 offset-xl-1">
-                  <h1 class="title underlined">Про компанию</h1>
-                  <div class="about__text">
-                      Наша компания уже больше десяти лет дарит позитивные эмоции детям и их родителям. Мы воплощаем все детские мечты и помогаем родителям дарить счастливое детство!
-                      <br> <br>
-                      Но и взрослые иногда так нуждаются в детских эмоциях! Мы можем и это, ведь так приятно почувствовать заботу даже когда тебе за... :)
-                  </div>
-                  <a href="#" class="button">Узнать больше</a>
-              </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 offset-md-1 col-lg-5 offset-lg-1">
+          <div class="about__img">
+            <img src="<?php the_field('section-about_image') ?>" alt="про компанию">
           </div>
+          <div class="about__img">
+            <?php
+            $image = get_field('section-about_image_2');
+
+            if (!empty($image)): ?>
+              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" width="<?php echo $image['width']; ?>" height="<?php echo $image['height']; ?>">
+            <?php endif;
+            ?>
+          </div>
+        </div>
+        <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-6 col-xl-5 offset-xl-1">
+          <h1 class="title underlined">
+            <?php the_field('section-about_title'); ?>
+          </h1>
+          <div class="about__text rte">
+            <?php the_field('section-about_description'); ?>
+          </div>
+          <a href="<?php the_field('section-about_link'); ?>" class="button">
+            <?php the_field('section-about_link_text'); ?>
+          </a>
+        </div>
       </div>
+    </div>
   </div>
 
   <div class="specialists" id="specialists">
