c0c649557888a9c9aad9d53060a97831b5fffab4
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2023-08-09 17:13:13 +0300

    add section Развивающие игрушки

--- wp-content/themes/childhood/index.php
+++ wp-content/themes/childhood/index.php
@@ -238,34 +238,49 @@ role="main"
 
       <h2 class="subtitle">Развивающие игрушки</h2>
       <div class="toys__wrapper">
+        <?php
+        $my_posts = get_posts(
+          array(
+            'numberposts' => -1,
+            'category_name' => 'soft_toys',
+            'orderby' => 'date',
+            'order' => 'ASC',
+            'post_type' => 'post',
+            'suppress_filters' => true,
+          )
+        );
 
-        <div
-        class="toys__item"
-        style="background-image: url(<?php echo bloginfo('template_url'); ?>/assets/img/toy_7.jpg)"
-        >
-          <div class="toys__item-info">
-            <div class="toys__item-title">Воздушный змей</div>
-            <div class="toys__item-descr">
-              Кто в детстве не хотел научиться летать? А змей поможет поймать
-              ветер и унести все заботы далеко-далеко...
-            </div>
-            <div class="minibutton toys__trigger">Подробнее</div>
-          </div>
-        </div>
+        global $post;
 
-        <div
-        class="toys__item"
-        style="background-image: url(<?php echo bloginfo('template_url'); ?>/assets/img/toy_8.jpg)"
-        >
-          <div class="toys__item-info">
-            <div class="toys__item-title">Музыкальные</div>
-            <div class="toys__item-descr">
-              Попробуйте заинтересовать ребенка музыкой! Может в нем таится
-              будущий Джаред Лето!
+        foreach ($my_posts as $post) {
+          setup_postdata($post);
+          ?>
+          <div
+          class="toys__item"
+          style="background-image: url(
+            <?php
+            if (has_post_thumbnail()) {
+              the_post_thumbnail_url();
+            } else {
+              echo get_template_directory_uri() . '/assets/img/not-found.jpg';
+            }
+            ?>)"
+          >
+            <div class="toys__item-info">
+              <div class="toys__item-title">
+                <?php the_title(); ?>
+              </div>
+              <div class="toys__item-descr">
+                <?php the_field('toys_description'); ?>
+              </div>
+              <div class="minibutton toys__trigger">Подробнее</div>
             </div>
-            <div class="minibutton toys__trigger">Подробнее</div>
           </div>
-        </div>
+          <?php
+        }
+
+        wp_reset_postdata();
+        ?>
 
       </div>
       <div class="row">
